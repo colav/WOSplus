24c6d5359e1b4fe10085a53da871657e8865bdf9
Diego Restrepo <restrepo@udea.edu.co>
2018-10-01 19:09:18 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -38,7 +38,7 @@ def main():
         name="WOSplus",
 
         # Version number (initial):
-        version="0.1.6",
+        version="0.1.7",
 
         # Application author details:
         author="Diego Restrepo",
